542fb4403100f99ab70a70820378eb0b68260c24
laurent dehon <laurent.dehon@gmail.com>
2025-09-28 11:23:00 +0200

    Add CheckBannedIp middleware to guest routes for enhanced security
    - 🔒 Implemented CheckBannedIp middleware to restrict access for banned IPs
    - 🚀 Updated guest route middleware to include IP banning functionality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\LocaleMiddleware;
+use App\Http\Middleware\CheckBannedIp;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         // Enregistrement du middleware LocaleMiddleware comme middleware global
         $middleware->web(LocaleMiddleware::class);
+        
+        // Enregistrement du middleware CheckBannedIp avec un alias
+        $middleware->alias([
+            'banned.ip' => CheckBannedIp::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
